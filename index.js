0df781c6e60fdc44f392def4d418a0a9a292aa25
동방진 <dbj@dongbangjin-ui-MacBookAir.local>
2022-11-17 08:56:28 +0900

    tcp comm.

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const net = require('net');
 
 const server = net.createServer(conn => {
     console.log('new client');
-
+    conn.write('Welcome!'+'\r\n');
     conn.on ('data', data => {
-        conn.write(data + '\r\n');
+        // conn.write(data + '\r\n');
         console.log(data);
+        console.log(data.toString());
+        conn.write("I've got your message: "+data+'\r\n')
     });
     
     conn.on('end', () => {
         console.log('client left');
     });
+    
 });
 
 server.listen(9090);
+
+
