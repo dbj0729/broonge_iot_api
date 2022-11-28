30de570f9a43a5efd1bca8edc5ede97e4bf19a48
동방진 <dbj@dongbangjin-ui-MacBookAir.local>
2022-11-28 14:09:28 +0900

    new

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const net = require('net');
-var seconds = (new Date()).getSeconds();
-console.log(seconds);
-const server = net.createServer(conn => {
-    console.log('new client');
-    conn.write('Welcome!'+'\r\n');
-    conn.on ('data', data => {
-        // conn.write(data + '\r\n');
-        console.log(data);
-        console.log(data.toString());
-        conn.write("I've got your message: "+data+'\r\n')
-        // const data2 = `hello`;
-        // const data_preparation = data2;
-        // const a = data_preparation.slice(1,3);
-        // console.log(a);
-    
-    });
+var net = require('net');
 
+// 서버를 생성
+var server = net.createServer(function(socket){
+	console.log(socket.address().address + " connected.");
+	
+	// client로 부터 오는 data를 화면에 출력
+	socket.on('data', function(data){
+		console.log('rcv:' + data);
+	});
+	// client와 접속이 끊기는 메시지 출력
+	socket.on('close', function(){
+		console.log('client disconnted.');
+	});
+	// client가 접속하면 화면에 출력해주는 메시지
+	socket.write('welcome to server');
+});
 
-    conn.on('end', () => {
-        console.log('client left');
-    });
+// 에러가 발생할 경우 화면에 에러메시지 출력
+server.on('error', function(err){
+	console.log('err'+ err	);
+});
 
+// Port 9090으로 접속이 가능하도록 대기
+server.listen(9090, function(){
+	console.log('linsteing on 5000..');
 });
