b5d5e94c0f91bf922566b5847a4d9714f794930d
동방진 <dbj@dongbangjin-ui-MacBookAir.local>
2022-12-05 11:14:54 +0900

    app sig added

--- index.js
+++ index.js
@@ -1,92 +1,85 @@
-var net = require("net");
+var net = require('net')
 // const readlinePromises = require("node:readline");
 // const rl = readlinePromises.createInterface({
 //   input: process.stdin,
 //   output: process.stdout,
 // });
 
-require("dotenv").config();
-const { connection } = require("./config/database");
+require('dotenv').config()
+const { connection } = require('./config/database')
 
-const IOT_PORT = process.env.IOT_PORT || "9090";
+const IOT_PORT = process.env.IOT_PORT || '9090'
 
 // IoT 에서 받는 Header byte size
-let size_1 = 4; // Sig.
-let size_2 = 4; // Group
-let size_3 = 1; // OP Code
-let size_4 = 10; // ID
-let size_5 = 5; // Version
-let size_6 = 2; // MSG Length
-let size_7 = 2; // Function_1
-let size_8 = 2; // Function_2
-let size_9 = 2; // Function_3
-let size_10 = 23; // Function_4
-let size_11 = 4; // Checksum
+let size_1 = 4 // Sig.
+let size_2 = 4 // Group
+let size_3 = 1 // OP Code
+let size_4 = 10 // ID
+let size_5 = 5 // Version
+let size_6 = 2 // MSG Length
+let size_7 = 2 // Function_1
+let size_8 = 2 // Function_2
+let size_9 = 2 // Function_3
+let size_10 = 23 // Function_4
+let size_11 = 4 // Checksum
 
 // Slice 로 진행하기에 그에 따른 글자 수에 따라 다음 단계를 불러오는 방식
-let sig_1 = size_1;
-let sig_2 = sig_1 + size_2;
-let sig_3 = sig_2 + size_3;
-let sig_4 = sig_3 + size_4;
-let sig_5 = sig_4 + size_5;
-let sig_6 = sig_5 + size_6;
-let sig_7 = sig_6 + size_7;
-let sig_8 = sig_7 + size_8;
-let sig_9 = sig_8 + size_9;
-let sig_10 = sig_9 + size_10;
-let sig_11 = sig_10 + size_11;
+let sig_1 = size_1
+let sig_2 = sig_1 + size_2
+let sig_3 = sig_2 + size_3
+let sig_4 = sig_3 + size_4
+let sig_5 = sig_4 + size_5
+let sig_6 = sig_5 + size_6
+let sig_7 = sig_6 + size_7
+let sig_8 = sig_7 + size_8
+let sig_9 = sig_8 + size_9
+let sig_10 = sig_9 + size_10
+let sig_11 = sig_10 + size_11
 
-
-let sockets = {};
-let bikeSocket;
+let sockets = {}
+let bikeSocket
 // 서버 생성
 var server = net.createServer(function (socket) {
-  console.log(socket.address().address + "Started Broonge IoT Server");
+  console.log(socket.address().address + 'Started Broonge IoT Server')
 
-    // client로 부터 오는 data를 화면에 출력
-    /* 
+  // client로 부터 오는 data를 화면에 출력
+  /* 
         Data 는 IoT 에서 받던, App 에서 받던 이래저래 같이 쓰이는 것이고
         아래 로직에서 차이가 나는 것이다.
     */
-    socket.on("data", async function (data) {
-    console.log("Received Data: " + data);
-    const data_elements = data.toString("utf-8").trim();
+  socket.on('data', async function (data) {
+    console.log('Received Data: ' + data)
+    const data_elements = data.toString('utf-8').trim()
 
+    // IoT 로부터 받는 정보이다.
+    const sig = data_elements.slice(0, sig_1)
+    const group = data_elements.slice(sig_1, sig_2)
+    const op_code = data_elements.slice(sig_2, sig_3)
+    const bike_id_from_iot = data_elements.slice(sig_3, sig_4)
+    const version = data_elements.slice(sig_4, sig_5)
+    const message_length = data_elements.slice(sig_5, sig_6)
 
-// IoT 로부터 받는 정보이다.
-    const sig = data_elements.slice(0, sig_1);
-    const group = data_elements.slice(sig_1, sig_2);
-    const op_code = data_elements.slice(sig_2, sig_3);
-    const bike_id_from_iot = data_elements.slice(sig_3, sig_4);
-    const version = data_elements.slice(sig_4, sig_5);
-    const message_length = data_elements.slice(sig_5, sig_6);
+    const f_1_battery = data_elements.slice(sig_6, sig_7)
+    const f_2_device_status = data_elements.slice(sig_7, sig_8)
+    const f_3_err_info = data_elements.slice(sig_8, sig_9)
+    const f_4_gps = data_elements.slice(sig_9, sig_10)
+    const gps_reformatted = f_4_gps.split('N') // 이 부분이 IoT 좌표에서 넘어올 때 구분되어 지는 값이다.
+    const f_4_lat = gps_reformatted[0].slice(0, 10) // 딱 10자리만 가져온다.
+    const f_4_lng = gps_reformatted[0].slice(0, 10) // 딱 10자리만 가져온다.
 
-    const f_1_battery = data_elements.slice(sig_6, sig_7);
-    const f_2_device_status = data_elements.slice(sig_7, sig_8);
-    const f_3_err_info = data_elements.slice(sig_8, sig_9);
-    const f_4_gps = data_elements.slice(sig_9, sig_10);
-    const gps_reformatted = f_4_gps.split("N"); // 이 부분이 IoT 좌표에서 넘어올 때 구분되어 지는 값이다.
-    const f_4_lat = gps_reformatted[0].slice(0,10); // 딱 10자리만 가져온다.
-    const f_4_lng = gps_reformatted[0].slice(0,10); // 딱 10자리만 가져온다.
-    
-    const checksum = data_elements.slice(sig_10, sig_11);
+    const checksum = data_elements.slice(sig_10, sig_11)
 
     // 변경되는 값; 이 부분을 저장해야 한다.
-    let manual_codes = f_1_battery + f_2_device_status + f_3_err_info + f_4_gps;
-// IoT 로부터 받는 정보 끝
+    let manual_codes = f_1_battery + f_2_device_status + f_3_err_info + f_4_gps
+    // IoT 로부터 받는 정보 끝
 
-//sockets 객체에 key 는 자전거 아이디 value 는 socket 을 할당한다.
-sockets[bike_id_from_iot] = socket;
-      
+    //sockets 객체에 key 는 자전거 아이디 value 는 socket 을 할당한다.
+    sockets[bike_id_from_iot] = socket
 
+    if (sig == process.env.IOT_SIG && group == process.env.IOT_GROUP && manual_codes.length !== 0) {
+      const combined_manual_codes = manual_codes.split('') // data 에서 온 raw 값을 글자 단위로 쪼갠 결과
+      const manual_codes_value = combined_manual_codes.map(item => item.charCodeAt()).reduce((acc, curr) => acc + curr) // 쪼갠 결과를 하나씩 분배
 
-    
-    if (manual_codes.length !== 0) {
-      const combined_manual_codes = manual_codes.split(""); // data 에서 온 raw 값을 글자 단위로 쪼갠 결과
-      const manual_codes_value = combined_manual_codes
-        .map((item) => item.charCodeAt())
-        .reduce((acc, curr) => acc + curr); // 쪼갠 결과를 하나씩 분배
-      
       /* 
       console.log({ manual_codes_value });
       console.log(manual_codes_value.toString(16));
@@ -98,32 +91,28 @@ sockets[bike_id_from_iot] = socket;
       // 만약, Checksum 이 다른 경우에는 데이터를 버려버린다. IoT 에 Return 할 필요는 없다.
       // 단, 만약, 20회 이상 Checksum 오류가 나는 경우에는 관리자에게 안내를 해 줘야 한다.
       // 상기 안내를 위해서 별도의 안내 방법이 필요할 수도 있다.
-      manual_codes_value_verification = manual_codes_value.toString(16); // 분배된 값을 16진수로 변경
+      manual_codes_value_verification = manual_codes_value.toString(16) // 분배된 값을 16진수로 변경
 
-      manually_added_0x = "0" + manual_codes_value_verification; // 마지막 checksum 에 0이 빠져서 0을 넣음
-      console.log(manually_added_0x);
+      manually_added_0x = '0' + manual_codes_value_verification // 마지막 checksum 에 0이 빠져서 0을 넣음
+      console.log(manually_added_0x)
 
-      
-
-      if (checksum == manually_added_0x) { // IoT 로 부터 받은 값이 모두 문제 없이 다 통과했을 때 실행
+      if (checksum == manually_added_0x) {
+        // IoT 로 부터 받은 값이 모두 문제 없이 다 통과했을 때 실행
         try {
-            if (bike_id_from_iot) {
-                //자전거가 보낸 통신일 경우
-                console.time("findBike Perfomance Time");
-                // --- 여기가 원래 sockets.[bike_id_from_iot] = socket; 이 있던 자리 ---
-                //DB에 해당 자전거 ID가 등록되어 있는지 확인
-                const findBikeQuery = `SELECT * FROM iot_status WHERE bike_id = ? limit 1`;
-                var [findBike] = await (
-                  await connection()
-                ).execute(findBikeQuery, [bike_id_from_iot]);
-                console.timeEnd("findBike Perfomance Time");
-          
-                if (findBike.length === 0) {
-                console.log("Bike you are searching for is not connected..")
-                }
-                else {
-                    console.time("Change Perfomance Time");
-                    const updateBikeStatusQuery = `
+          if (bike_id_from_iot) {
+            //자전거가 보낸 통신일 경우
+            console.time('findBike Perfomance Time')
+            // --- 여기가 원래 sockets.[bike_id_from_iot] = socket; 이 있던 자리 ---
+            //DB에 해당 자전거 ID가 등록되어 있는지 확인
+            const findBikeQuery = `SELECT * FROM iot_status WHERE bike_id = ? limit 1`
+            var [findBike] = await (await connection()).execute(findBikeQuery, [bike_id_from_iot])
+            console.timeEnd('findBike Perfomance Time')
+
+            if (findBike.length === 0) {
+              console.log('Bike you are searching for is not found..') // 등록되지 않은 자전거이다.
+            } else {
+              console.time('Change Perfomance Time')
+              const updateBikeStatusQuery = `
                     UPDATE iot_status SET 
                         battery = ${f_1_battery},
                         lat = ${f_4_lat},
@@ -131,127 +120,120 @@ sockets[bike_id_from_iot] = socket;
                         signal_strength = "1",
                         led = "on",
                         status = 'current status'
-                        WHERE bike_id = ?`;
-                    await (await connection()).execute(updateBikeStatusQuery, [bike_id_from_iot]);
-                    sockets[bike_id_from_iot].write('Welcome! '+bike_id_from_iot);
-                    console.timeEnd("Change Perfomance Time");
-                  }
-              } else {
-                //앱에서 보낸 통신일 경우
-                //sockets 객체 안에 bikeId에 해당하는 키값이 있는지 판별한다.
-                if (!sockets[bike_id_from_iot]) socket.write("The requested bike id is not registered in the server array.");
-              }
+                        WHERE bike_id = ?`
+              await (await connection()).execute(updateBikeStatusQuery, [bike_id_from_iot])
+              sockets[bike_id_from_iot].write('Welcome! ' + bike_id_from_iot)
+              console.timeEnd('Change Perfomance Time')
+            }
+          } else {
+            //앱에서 보낸 통신일 경우
+            //sockets 객체 안에 bikeId에 해당하는 키값이 있는지 판별한다.
+            if (!sockets[bike_id_from_iot]) socket.write('The requested bike id is not registered in the server array.')
+          }
         } catch (error) {
-          console.error(error);
+          console.error(error)
         }
       } else {
         try {
-          console.log("Bad");
-          socket.write(`Wrong type of data transaction.`); // 상기 횟수에 따라 오류가 발생할 경우, 관리자 Alert 를 띄워야 한다.
+          console.log('Bad')
+          socket.write(`Wrong type of data transaction.`) // 상기 횟수에 따라 오류가 발생할 경우, 관리자 Alert 를 띄워야 한다.
         } catch (error) {
-          console.error(error);
+          console.error(error)
         }
       }
-    } else { // 이 부분이 IoT 로 보내기 위해 App 으로부터 받는 부분이다.
+    } else {
+      // 이 부분이 IoT 로 보내기 위해 App 으로부터 받는 부분이다.
 
-        // App 에서 IoT 로 보내기 위해 받는 Protocol
-        let app_to_iot_data = data_elements.split(",");
-        console.log("Here is the app to iot data: "+app_to_iot_data[0])
+      // App 에서 IoT 로 보내기 위해 받는 Protocol
+      let app_to_iot_data = data_elements.split(',')
+      console.log('Here is the app to iot data: ' + app_to_iot_data[1])
 
+      // App 에서 IoT 로 정보를 보내기 위한 기본 변수들이다.
+      var sig_for_app = process.env.IOT_SIG
+      var group_for_app = process.env.IOT_GROUP
+      var op_code_for_app = '3' // 3번이 보내는 경우이다.
+      var bike_id_for_app = app_to_iot_data[1]
+      var version_for_app = 'V0.31'
+      var message_length_for_app = '35'
+      var send_default_data_preparation =
+        sig_for_app + group_for_app + op_code_for_app + bike_id_for_app + version_for_app + message_length_for_app
 
-        // App 에서 IoT 로 정보를 보내기 위한 기본 변수들이다.
-        var sig_for_app = "1934";
-        var group_for_app = "BR01";
-        var op_code_for_app = "3"; // 3번이 보내는 경우이다.
-        var bike_id_for_app = app_to_iot_data[0];
-        var version_for_app = "V0.31";
-        var message_length_for_app = "35";
-        var send_default_data_preparation =
-            sig_for_app +
-            group_for_app +
-            op_code_for_app +
-            bike_id_for_app +
-            version_for_app +
-            message_length_for_app;
+      function sending_codes(send_code) {
+        var send_codes = send_default_data_preparation + send_code
+        var combined_send_codes = send_codes.split('')
+        var send_codes_value = combined_send_codes.map(item => item.charCodeAt()).reduce((acc, curr) => acc + curr)
+        var send_codes_value_verification = send_codes_value.toString(16)
+        var send_codes_manually_added_0x = '0' + send_codes_value_verification
+        var final_send_codes = send_codes + send_codes_manually_added_0x
+        return final_send_codes
+      }
 
-        function sending_codes(send_code) {
-            var send_codes = send_default_data_preparation + send_code;
-            var combined_send_codes = send_codes.split("");
-            var send_codes_value = combined_send_codes
-                .map((item) => item.charCodeAt())
-                .reduce((acc, curr) => acc + curr);
-            var send_codes_value_verification = send_codes_value.toString(16);
-            var send_codes_manually_added_0x = "0" + send_codes_value_verification;
-            var final_send_codes = send_codes + send_codes_manually_added_0x;
-            return final_send_codes;
-        }
-
-
-        // bikeSocket = app_to_iot_data[0];
+      if (app_to_iot_data[0] == process.env.APP_SIG) {
+        // bikeSocket = app_to_iot_data[1];
         // sockets[bike_id_from_iot] = socket; // App 에서 보내려고하는데 그 자전거가 붙어 있는지도 확인해야 하는 과정...?
         // console.log("================"+JSON.stringify(sockets[bike_id_from_iot]))
 
-// 그 자전거가 붙어있는지 여부를 확인하는 과정이 필요하다. query 를 반드시 돌려야만 하는가? 매번? 아님 한 번? 
+        // 그 자전거가 붙어있는지 여부를 확인하는 과정이 필요하다. query 를 반드시 돌려야만 하는가? 매번? 아님 한 번?
 
-        if (app_to_iot_data[1] == "unlock") {
-            try {
-                // 01 이면 잠금해제 이다.
-                const send_code = "01";
-                // 여기서 실제로 IoT 에서 받는 값을 보고 진짜로 잠겼는지 열렸는지 확인해야 한다.
-                socket.write(sending_codes(send_code), "utf8");
-                console.time("Change Perfomance Time");
-                const updateBikeStatusQuery = `UPDATE iot_status SET status = 'unlocked' WHERE bike_id = ?`;
-                await (await connection()).execute(updateBikeStatusQuery, [bike_id_for_app]);
-                sockets[bike_id_from_iot].write('unlocked!');
-                socket.write("success, enjoy your bike ride");
-                console.timeEnd("Change Perfomance Time");
-            } catch (error) {
-                console.error(error);
-              }
-        } else if (app_to_iot_data[1] == "lock") {
-
-            // 00 이면 잠금 이다.
-            const send_code = "00";
+        if (app_to_iot_data[2] == 'unlock') {
+          try {
+            // 01 이면 잠금해제 이다.
+            const send_code = '01'
             // 여기서 실제로 IoT 에서 받는 값을 보고 진짜로 잠겼는지 열렸는지 확인해야 한다.
-            // 1. IoT 가 접속 중인지를 확인한다.
-            // 2. Status 가 locked 인지를 확인한다.
-            // 3. locked 이면 굳이 실행시킬 필요는 없다. 그렇다고 굳이 또 안 돌릴 이유도 없다.
-            // 4. IoT --> 현상태 확인 --> IoT 에게 명령 --> 이 후 success 받으면 Server 에 저장 (이것이 정석이다)
-            socket.write(sending_codes(send_code), "utf8");
-            console.time("Change Perfomance Time");
-            const updateBikeStatusQuery = `UPDATE iot_status SET status = 'locked' WHERE bike_id = ?`;
-            await (await connection()).execute(updateBikeStatusQuery, [bike_id_for_app]);
-            sockets[bike_id_from_iot].write('locked!'); // 이거? 아래꺼? 어떤걸로 보내야 그 IoT 로 보낼 수 있는 것인가?
-            socket.write("Thank you for your riding Broonge!");
-            console.timeEnd("Change Perfomance Time");
+            socket.write(sending_codes(send_code), 'utf8')
+            console.time('Change Perfomance Time')
+            const updateBikeStatusQuery = `UPDATE iot_status SET status = 'unlocked' WHERE bike_id = ?`
+            await (await connection()).execute(updateBikeStatusQuery, [bike_id_for_app])
+            sockets[bike_id_from_iot].write('unlocked!') // IoT 에 보내는 소켓
+            socket.write('success, enjoy your bike ride') // 사용자 앱
+            console.timeEnd('Change Perfomance Time')
+          } catch (error) {
+            console.error(error)
+          }
+        } else if (app_to_iot_data[2] == 'lock') {
+          // 00 이면 잠금 이다.
+          const send_code = '00'
+          // 여기서 실제로 IoT 에서 받는 값을 보고 진짜로 잠겼는지 열렸는지 확인해야 한다.
+          // 1. IoT 가 접속 중인지를 확인한다.
+          // 2. Status 가 locked 인지를 확인한다.
+          // 3. locked 이면 굳이 실행시킬 필요는 없다. 그렇다고 굳이 또 안 돌릴 이유도 없다.
+          // 4. IoT --> 현상태 확인 --> IoT 에게 명령 --> 이 후 success 받으면 Server 에 저장 (이것이 정석이다)
+          socket.write(sending_codes(send_code), 'utf8')
+          console.time('Change Perfomance Time')
+          const updateBikeStatusQuery = `UPDATE iot_status SET status = 'locked' WHERE bike_id = ?`
+          await (await connection()).execute(updateBikeStatusQuery, [bike_id_for_app])
+          sockets[bike_id_from_iot].write('locked!') // 이거? 아래꺼? 어떤걸로 보내야 그 IoT 로 보낼 수 있는 것인가?
+          socket.write('Thank you for your riding Broonge!')
+          console.timeEnd('Change Perfomance Time')
         }
+      }
     }
-  });
+  })
   // client와 접속이 끊기는 메시지 출력
-  socket.on("close", function () {
+  socket.on('close', function () {
     // 연결을 끊는 socket이 sockets 안에 등록된 socket인지 판별한다.
-    const findBikeId = Object.entries(sockets).find((s) => s[1] === socket);
+    const findBikeId = Object.entries(sockets).find(s => s[1] === socket)
     if (findBikeId) {
-      console.log("bikeSocket disconnected");
-      delete sockets[findBikeId[0]];
-      return;
+      console.log('bikeSocket disconnected')
+      delete sockets[findBikeId[0]]
+      return
     }
-    console.log("Client has left the IoT Server.");
-  });
+    console.log('Client has left the IoT Server.')
+  })
   // client가 접속하면 화면에 출력해주는 메시지
   // socket.write("Welcome to the IoT Server");
-});
+})
 
 // 에러가 발생할 경우 화면에 에러메시지 출력
-server.on("error", function (err) {
-  if (bikeSocket) delete sockets[bike_id_from_iot];
-  console.log("err" + err);
-});
+server.on('error', function (err) {
+  if (bikeSocket) delete sockets[bike_id_from_iot]
+  console.log('err' + err)
+})
 
 // .env 의 포트값으로 진행되던가 아니면 9090 으로 진행되던가 해서 접속이 가능하도록 대기
 server.listen(IOT_PORT, function () {
-  console.log(`Listening for requests on port ${IOT_PORT}`);
-});
+  console.log(`Listening for requests on port ${IOT_PORT}`)
+})
 
 /*
 3자리이면 앞에 0 한개 붙이시고,,,,,2자리 나오면 앞에 0 2개 이렇게
@@ -261,4 +243,3 @@ checksum length 를 count 하고,
 그 length 에 있어서 자리수가 부족한 경우에는 0으로 매꾼다???
 
 */
-
