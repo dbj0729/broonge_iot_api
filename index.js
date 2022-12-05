2684253c3db1b4a5d22c5031b78aaaf5da519fbc
MinHwan <gozld5153@naver.com>
2022-12-05 20:51:42 +0900

    rollback

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ var server = net.createServer(function (socket) {
     // 변경되는 값; 이 부분을 저장해야 한다.
     let manual_codes = f_1_gps + f_2_signal_strength + f_3_battery + f_4_device_status + f_5_err_info
     // IoT 로부터 받는 정보 끝
-    // if (sig === process.env.IOT_SIG) sockets[bike_id_from_iot] = socket
+    if (sig === process.env.IOT_SIG) sockets[bike_id_from_iot] = socket
 
     if (sig == process.env.IOT_SIG && group == process.env.IOT_GROUP && manual_codes.length !== 0) {
       const combined_manual_codes = manual_codes.split('') // data 에서 온 raw 값을 글자 단위로 쪼갠 결과
