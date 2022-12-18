63c65e3496a6fc6a38a3d904347a27e192a44119
동방진 <dbj@172.18.nate.com>
2022-12-18 10:38:06 +0900

    testing

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ var net = require('net')
 // });
 function getCurrentTime() {
   var adjust_time_manual = 9 * 60 * 60 * 1000
-  const datetime_in_number = Number(new Date())
+  const datetime_in_number = Number(new Date()) + adjust_time_manual
   const datetime = new Date(datetime_in_number).toLocaleString('ko-KR')
   const result = String(datetime)
   console.log(result)
+  console.log(datetime_in_number)
   return result
 }
 console.log(getCurrentTime())
