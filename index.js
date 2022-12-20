a374b28990047137aa85c144d7535baa3c945cb8
MinHwan <gozld5153@naver.com>
2022-12-20 16:18:13 +0900

    add console

--- index.js
+++ index.js
@@ -285,11 +285,9 @@ var server = net.createServer(function (socket) {
         // }
         console.log({ toBikeCode: sending_codes(code) })
         console.log('appSocket : order is ' + order)
-        sockets[app_to_iot_data[1]].write(sending_codes(code)) // @DBJ 이 부분 점검 필요?
+        const isSending = sockets[app_to_iot_data[1]].write(sending_codes(code)) // @DBJ 이 부분 점검 필요?
         console.log('sending codes : ' + sending_codes(code))
-        console.log(
-          '---------------success sending------------------------------------------------' + app_to_iot_data[1],
-        )
+        console.log('---------------success sending??????????????????????????????' + isSending)
 
         socket.write(sending_codes(code))
         socket.write('   ') // App 한테 보내는 것
