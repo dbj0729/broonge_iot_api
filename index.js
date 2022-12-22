53e5a4969f0b1a1a22f246e4351a94518b68e10c
MinHwan <gozld5153@naver.com>
2022-12-22 19:31:08 +0900

    awef

--- index.js
+++ index.js
@@ -319,7 +319,7 @@ var server = net.createServer(async function (socket) {
             while (sock.readyState !== 'open') {
               setTimeout(() => console.log('waiting'), 100)
             }
-            await new Promise(resolve => setTimeout(resolve, 10))
+            // await new Promise(resolve => setTimeout(resolve, 10))
             sock.write(sending_codes(code), () => console.log('socketState :' + sock.readyState))
           }
         }
