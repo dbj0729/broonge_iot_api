709998f6da0bcfbae055c99b93f6ce94794a1bea
MinHwan <gozld5153@naver.com>
2023-01-16 14:51:58 +0900

    del socket delay

--- index.js
+++ index.js
@@ -100,7 +100,6 @@ var server = net.createServer(async function (socket) {
     */
     // let bike_id_from_iot
     console.log(socket.address().address + ' Started Broonge IoT Server on ' + getCurrentTime())
-    socket.setNoDelay(false)
     socket.id = socket.remoteAddress + ':' + socket.remotePort
     const [value, release] = await semaphore.acquire()
     try {
