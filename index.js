20cc4fdcf86b534a39192e9650f354540f1af4cd
MinHwan <gozld5153@naver.com>
2023-01-16 18:47:30 +0900

    del console

--- index.js
+++ index.js
@@ -214,8 +214,6 @@ var server = net.createServer(async function (socket) {
                 bike_id_from_iot,
               ])
 
-              console.log('update complete ' + bike_id_from_iot, result)
-
               if (f_4_device_status === '00') {
                 const selectBikeRiding = `SELECT * FROM riding_data WHERE bike_id = ? AND start_datetime IS NOT NULL AND end_datetime IS NULL ORDER BY id DESC LIMIT 1`
                 const [selectResult] = await (await connection()).query(selectBikeRiding, [bike_id_from_iot])
