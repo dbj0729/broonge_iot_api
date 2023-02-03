c7e62914d5b27671ad171f01c1a2630eb6a9f1fe
MinHwan <gozld5153@naver.com>
2023-02-03 17:06:50 +0900

    add console

--- index.js
+++ index.js
@@ -138,7 +138,9 @@ var server = net.createServer(async function (socket) {
 
       socket.on('data', async function (data) {
         const data_elements = data.toString('utf-8').trim()
+        console.log(data_elements)
         // BR01486867506000147608930???????????????????????59903990725
+        //
         // console.log('연결된 자전거 ' + Object.keys(sockets))
         // IoT 로부터 받는 정보이다.
         const sig = data_elements.slice(0, sig_1)
