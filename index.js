74febc837592fefdeef35a88f09a69cc76319389
MinHwan <gozld5153@naver.com>
2023-02-06 16:16:14 +0900

    remark firmware upgrade

--- index.js
+++ index.js
@@ -94,6 +94,36 @@ let beforeSendBikeId = ''
 let duplicateGPS = {}
 let failUpdate = 0
 
+// fs.readFile('CH32V203C8T6.bin', (err, data) => {
+//   if (err) console.log(err)
+//   const max = Math.floor(data.length / 1025)
+
+//   var sig_for_app = process.env.IOT_SIG
+//   var group_for_app = process.env.IOT_GROUP
+//   var op_code_for_app = '3' // 3번이 보내는 경우이다.
+
+//   var version_for_app = 'APP'
+//   var message_length_for_app = '1024' //IOT_ERROR_MESSAGE_LENGTH???
+//   var send_default_data_preparation =
+//     sig_for_app + group_for_app + op_code_for_app + 'test' + version_for_app + message_length_for_app
+
+//   for (let i = 0; i <= max; i++) {
+//     const buf1 = new Buffer(1024)
+//     const sendBuf = data.copy(buf1, 0 * 1025, (i + 1) * 1025)
+//     let checksum = 0
+//     for (let j = 0; j < sendBuf.length; i++) {
+//       checksum += sendBuf[j]
+//     }
+
+//     // console.log(send_default_data_preparation + sendBuf)
+//     // sockets[bike_id_from_iot].write(send_default_data_preparation + sendBuf + checksum)
+//   }
+
+//   let lastBuffer = data.slice(max * 1025, data.length)
+//   console.log(lastBuffer)
+//   // sockets[bike_id_from_iot].write(lastBuffer)
+// })
+
 // 서버 생성
 var server = net.createServer(async function (socket) {
   const release1 = await mutex.acquire()
@@ -107,7 +137,7 @@ var server = net.createServer(async function (socket) {
 
     console.log(socket.address().address + ' Started Broonge IoT Server on ' + getCurrentTime())
     socket.setNoDelay(true)
-    // socket.setKeepAlive(true, 2000)
+    socket.setKeepAlive(true, 2000)
 
     socket.id = socket.remoteAddress + ':' + socket.remotePort
 
@@ -180,20 +210,36 @@ var server = net.createServer(async function (socket) {
         const message_length = data_elements.slice(sig_5, sig_6)
 
         //TODO: 펌웨어 업그레이드 test
-        if (bike_id_from_iot === '1223129999') {
-          fs.readFile('CH32V203C8T6.bin', (err, data) => {
-            if (err) console.log(err)
-            const max = Math.floor(data.length / 1025)
+        // if (bike_id_from_iot === '1223129999') {
+        //   fs.readFile('CH32V203C8T6.bin', (err, data) => {
+        //     if (err) console.log(err)
+        //     const max = Math.floor(data.length / 1025)
 
-            for (let i = 0; i <= max; i++) {
-              const buf1 = new Buffer(1024)
-              sockets[bike_id_from_iot].write(data.copy(buf1, 0 * 1025, (i + 1) * 1025))
-            }
+        //     var sig_for_app = process.env.IOT_SIG
+        //     var group_for_app = process.env.IOT_GROUP
+        //     var op_code_for_app = '3' // 3번이 보내는 경우이다.
 
-            let lastBuffer = data.slice(max * 1025, data.length)
-            sockets[bike_id_from_iot].write(lastBuffer)
-          })
-        }
+        //     var version_for_app = 'APP'
+        //     var message_length_for_app = '1024' //IOT_ERROR_MESSAGE_LENGTH???
+        //     var send_default_data_preparation =
+        //       sig_for_app +
+        //       group_for_app +
+        //       op_code_for_app +
+        //       bike_id_from_iot +
+        //       version_for_app +
+        //       message_length_for_app
+
+        //     for (let i = 0; i <= max; i++) {
+        //       const buf1 = new Buffer(1024)
+        //       const sendBuf = data.copy(buf1, 0 * 1025, (i + 1) * 1025)
+        //       const checksum = sendBuf.reduce((acc, curr) => acc + curr).toString(16)
+        //       sockets[bike_id_from_iot].write(send_default_data_preparation + sendBuf + checksum)
+        //     }
+
+        //     let lastBuffer = data.slice(max * 1025, data.length)
+        //     sockets[bike_id_from_iot].write(lastBuffer)
+        //   })
+        // }
 
         // console.log('version: ' + version)
         // console.log('message_length: ' + message_length)
