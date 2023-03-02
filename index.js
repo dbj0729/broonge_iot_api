ee1b0b4ee89af5dbccd17d51bce513cf110d4eb7
MinHwan <gozld5153@naver.com>
2023-03-02 17:02:37 +0900

    firmware update test15

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const IOT_PORT = process.env.IOT_PORT || '8000'
 //TODO: firmware upgrade
 const FILE = fs.readFileSync('CH32V203C8T6.bin')
 let max = Math.ceil(FILE.length / 1024)
-let lastBuffer = Buffer.alloc(105)
+let lastBuffer = Buffer.alloc(50)
 // let char = 2
-for (let i = 0; i < 105; i++) {
+for (let i = 0; i < 50; i++) {
   lastBuffer[i] = FILE[i]
 }
 
