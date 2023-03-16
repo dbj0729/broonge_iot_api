c4902866d40f90a961c9dce2d526f75f5d8690d6
MinHwan <gozld5153@naver.com>
2023-03-16 16:49:06 +0900

    fix setting

--- index.js
+++ index.js
@@ -43,8 +43,6 @@ var server = net.createServer(async function (socket) {
     console.log(socket.address().address + ' Started Broonge IoT Server on ' + getCurrentTime())
     socket.setNoDelay(true)
     socket.setKeepAlive(true)
-    socket.setSendBufferSize(1024 * 1024)
-    socket.setReceiveBufferSize(1024 * 1024)
 
     socket.id = socket.remoteAddress + ':' + socket.remotePort
 
